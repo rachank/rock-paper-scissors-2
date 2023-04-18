75903ea1b1353fe87820ca8d9afdc0491df22acd
rachank <rhanka@protonmail.com>
2023-04-18 08:16:41 +0000

    add computer random choice of image function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,19 @@ function playGame(e) {
 // Event Listener for each initial click on start images to play game
 startImages.forEach(startimage => startimage.addEventListener('click', playGame));
 
+//Get computer random choice of image for game function
+function findCompPick() {
+    const randPick = Math.floor(Math.random() * 3) + 1;
+    switch (randPick) {
+        case 1:
+            compPick = "rock";
+            break;
+        case 2:
+            compPick = "paper";
+            break;
+        case 3:
+            compPick = "scissors";
+            break;
+    }
+}
+

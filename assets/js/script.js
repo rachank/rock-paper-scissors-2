90834d7d081776ba3a04381a94e8c0061e359304
rachank <rhanka@protonmail.com>
2023-04-18 08:12:27 +0000

    addconstants for DOM to js code

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/*List all constants for DOM elements and game options */
+
+let startImages = document.querySelectorAll(".startimage");
+let compPick;
+//const winnerResult = document.getElementById("winnerResult"); 
+//add above to HTML Modal div
+const modal = document.getElementsByClassName(".modal")
